1ab774e133c93eadf519e0defa28020c90fd1a59
lincolnmay01 <ljmay002@louisville.edu>
2024-12-23 14:34:04 -0500

    contact updates

--- script.js
+++ script.js
@@ -12,7 +12,4 @@ document.getElementById("githubbutton").addEventListener("click", function() {
   window.open(linkedInUrl, "_blank");
 });
 
-    window.onload = function() {
-        // Reset the form fields when the page loads
-        document.getElementById("contact-form").reset();
-    };
+  
